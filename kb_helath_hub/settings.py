c1714bcd0a1dda32b3a4f02d96e141d06d00e316
aine jimson <jimson@debian-BULLSEYE-live-builder-AMD64>
2023-11-26 13:27:31 +0300

    chaged debug settings

--- kb_helath_hub/settings.py
+++ kb_helath_hub/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9i=^cu9$qk7!yj4)$djcdrc=3js4_s+1^popc$xg250l1t(%5p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
